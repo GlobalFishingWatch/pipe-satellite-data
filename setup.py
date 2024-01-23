2ac82fbe6b46fe9c9603dbc4271b5e533ace9207
Matias Piano <matias@globalfishingwatch.org>
2024-01-23 13:12:33 -0300

    Updates to py-3.8 and updates all the required dependencies with latest versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pipe-satellite-data',
-    version=__import__('pipe_satellite_data').__version__,
+    version='3.1.0',
     packages=find_packages(exclude=['test*.*', 'tests'])
 )
 
